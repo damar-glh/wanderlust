f574f2e0dbbf40a26247e1ace0181d27205cc432
Damar Galih <damarmuhamad13@gmail.com>
2024-10-03 02:01:19 +0700

    feat: home page with navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ["./dist/**/*.{html,js}"],
+    theme: {
+        extend: {
+            fontFamily: {
+                'sans': ['Roboto', 'sans-serif'],
+            },
+            colors: {
+                'color-pallet': {
+                    'primary': '#F9F9F9',
+                    'secondary': '#F5F5F5',
+                    'tertiary': '#E0E0E0',
+                    'quaternary': '#BDBDBD',
+                    'quinary': '#828282',
+                    'senary': '#4F4F4F',
+                    'septenary': '#333333',
+                    'octonary': '#1A1A1A',
+                    'nonary': '#121212',
+                    'denary': '#000000',
+                },
+            },
+        },
+    },
+    plugins: [],
 }
 
