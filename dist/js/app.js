97eb4d1c7c93867c75bd95a1c1860e72b7b4957d
Damar Galih <damarmuhamad13@gmail.com>
2024-10-04 21:15:06 +0700

    ref: active and unactive navlinks on click at navbar

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,17 +1,8 @@
 const nav = document.querySelector('nav');
 const btnNav = document.querySelector('.btn-nav');
 const navItems = document.querySelector('.nav-items');
+const navLinks = document.querySelectorAll('.nav-links');
 
-btnNav.addEventListener('click', function () {
-        if (navItems.classList.contains('hidden')) {
-            navItems.classList.remove('hidden');
-            navItems.classList.add('absolute', 'top-full', 'left-0', 'w-full', 'text-center', 'p-3', 'text-lg', 'gap-2');
-        } else {
-            navItems.classList.add('hidden');
-            navItems.classList.remove('absolute', 'top-full', 'left-0', 'w-full', 'text-center', 'p-3', 'text-lg', 'gap-2');
-        }
-    }
-);
 
 window.addEventListener('scroll', function () {
     const scrollHeight = window.scrollY;
@@ -23,3 +14,22 @@ window.addEventListener('scroll', function () {
         navItems.classList.remove('bg-black');
     }
 });
+
+navLinks.forEach(link => {
+    link.addEventListener('click', function (e) {
+            navLinks.forEach(l => l.classList.remove('active'));
+            this.classList.add('active');
+        }
+    )
+});
+
+btnNav.addEventListener('click', function () {
+        if (navItems.classList.contains('hidden')) {
+            navItems.classList.remove('hidden');
+            navItems.classList.add('absolute', 'top-full', 'left-0', 'w-full', 'text-center', 'p-3', 'text-lg', 'gap-2');
+        } else {
+            navItems.classList.add('hidden');
+            navItems.classList.remove('absolute', 'top-full', 'left-0', 'w-full', 'text-center', 'p-3', 'text-lg', 'gap-2');
+        }
+    }
+);
