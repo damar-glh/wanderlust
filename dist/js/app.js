f4d63d9410420448b7b02a3c8dd7e2b34507a877
Damar Galih <damarmuhamad13@gmail.com>
2024-10-04 20:22:57 +0700

    ref: responsif navbar for mobile

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,10 +1,25 @@
-window.addEventListener('scroll' , function(){
-    const nav = document.querySelector('nav');
-    const scrollHeight = window.scrollY;
+const nav = document.querySelector('nav');
+const btnNav = document.querySelector('.btn-nav');
+const navItems = document.querySelector('.nav-items');
 
-    if(scrollHeight > 50){
-        nav.classList.add('bg-black', 'bg-opacity-70');
+btnNav.addEventListener('click', function () {
+        if (navItems.classList.contains('hidden')) {
+            navItems.classList.remove('hidden');
+            navItems.classList.add('absolute', 'top-full', 'left-0', 'w-full', 'text-center', 'p-3', 'text-lg', 'gap-2');
+        } else {
+            navItems.classList.add('hidden');
+            navItems.classList.remove('absolute', 'top-full', 'left-0', 'w-full', 'text-center', 'p-3', 'text-lg', 'gap-2');
+        }
+    }
+);
+
+window.addEventListener('scroll', function () {
+    const scrollHeight = window.scrollY;
+    if (scrollHeight > 50) {
+        nav.classList.add('bg-black');
+        navItems.classList.add('bg-black');
     } else {
-        nav.classList.remove('bg-black', 'bg-opacity-70');
+        nav.classList.remove('bg-black');
+        navItems.classList.remove('bg-black');
     }
 });
